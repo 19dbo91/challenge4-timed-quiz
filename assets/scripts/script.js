80ce272fbdbc069aff68391e5aadedeceee89a6e
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-04-19 23:45:24 -0700

    added notes to .js file

---
+++ assets/scripts/script.js
@@ -0,0 +1,58 @@
+/*From README
+
+USR STORY
+_______________________________________________________________________________
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
+
+
+ACC CRIT
+_______________________________________________________________________________
+.:  I am taking a code quiz
+
+@ click the start button:
+    THEN a timer starts and I am presented with a question
+
+@ answer a question:
+    THEN I am presented with another question
+
+@ answer a question incorrectly:
+    THEN time is subtracted from the clock
+
+@ all questions are answered or the timer reaches 0:
+    THEN the game is over
+
+@ the game is over:
+    THEN I can save my initials and my score
+
+
+
+NOTES ON MOCK UP
+_______________________________________________________________________________
+
+hovers lightens box; changes mouse to finger
+highscore persists; can be cleared
+
+format tends to be fill-in the blank questions
+
+after click: instant feedback -> correct/wrong, shortly after disappears
+
+visible top lt: view high scores at any time
+visible top rt: time (starts at 75s... wrong -6s)
+
+on game over "page" -> form to give initials and score to board
+on hi-score "page" -> goback takes you to start "page"
+on start "page" -> go -> question page
+on question page -> options(4) -> next question -until out of questions/time-> 
+
+options are in ordered list and styled
+
+
+HTML and CSS exclusively on the fly?
+
+Extra features (if code done by weekend)
+    
+
+*/
