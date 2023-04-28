e607d23994755942f41edb0895a9f1d0b5543387
Dustin Bonilla <bonilla.dustin@gmail.com>
2023-04-27 18:04:54 -0700

    added leaderboard functions

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,115 +1,109 @@
-/*From README
-
-USR STORY
-_______________________________________________________________________________
-AS A coding boot camp student
-I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
-SO THAT I can gauge my progress compared to my peers
-
-
-
-ACC CRIT
-_______________________________________________________________________________
-.:  I am taking a code quiz
-
-@ click the start button: //! DONE !
-    THEN a timer starts and I am presented with a question
-
-@ answer a question: //! DONE !
-    THEN I am presented with another question
-
-@ answer a question incorrectly: //! DONE !
-    THEN time is subtracted from the clock
-
-@ all questions are answered or the timer reaches 0: //! DONE !
-    THEN the game is over
-
-@ the game is over: //TODO: ERROR
-    THEN I can save my initials and my score //local storage
-
-
-
-NOTES ON MOCK UP
-_______________________________________________________________________________
-
-hovers lightens box; changes mouse to finger
-highscore persists; can be cleared
-
-format tends to be fill-in the blank questions
-
-after click: instant feedback -> correct/wrong, shortly after disappears
-
-visible top lt: view high scores at any time
-visible top rt: time (starts at 75s... wrong -6s)
-
-on game over "page" -> form to give initials and score to board
-on hi-score "page" -> goback takes you to start "page"
-on start "page" -> go -> question page
-on question page -> options(4) -> next question -until out of questions/time-> 
-
-options are in ordered list and styled
-
-
-
-Other considerations: media screeen
-Extra features (if code done by weekend)
-    
-
-general flow of site
-
-on url load - starting page
-    show prompt centered
-    show view high score button
-    show timer
-    has start button (mid center) ->
-    has view highscore (top left) ->
-    has timer(top right; @ 000)
-
-on button start:
-    get question, ?randomly?
-    show list choices with button ->
-    start timer counting down every second (starting from ?75) ->
-
-on button choice
-    reveal a text below, timed for 2?secs    
-    update score;save it
-
-    (if more questions and time)
-        get next question from list
-        load its choices; ...wait for button choice
-    (else)
-        go to game end
-
-on game end
-    get score
-    show form intials and submit
-
-on submit
-    get leaderboard
-    place score in highest to lowest
-
-on view highscore
-    load leaderboard array of initials and score
-    on button; reset
-    go back to home ->
-
-on reset
-    delete value from local storage
-    reload display
-
+/*      USER STORY
+*_______________________________________________________________________________
+*AS A coding boot camp student
+*I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+*SO THAT I can gauge my progress compared to my peers
 */
 
-/* References
+/*      ACCEPTANCE CRITERIA
+*_______________________________________________________________________________
+*.:  I am taking a code quiz
+*
+* ////@ click the start button:
+* ////THEN a timer starts and I am presented with a question
+*
+* ////@ answer a question: //! DONE !
+* ////THEN I am presented with another question
+*
+* ////@ answer a question incorrectly: //! DONE !
+* ////THEN time is subtracted from the clock
+* 
+* ////@ all questions are answered or the timer reaches 0: //! DONE !
+* ////THEN the game is over
+* 
+* @ the game is over: //TODO: ERROR
+*      THEN I can save my initials and my score //local storage
+*/
+
+/*      NOTES ON MOCK UP
+* _______________________________________________________________________________
+* 
+* hovers lightens box; changes mouse to finger
+* highscore persists; can be cleared
+* 
+* format tends to be fill-in the blank questions
+* 
+* after click: instant feedback -> correct/wrong, shortly after disappears
+* 
+* visible top lt: view high scores at any time
+* visible top rt: time (starts at 75s... wrong -6s)
+
+* on game over "page" -> form to give initials and score to board
+* on hi-score "page" -> goback takes you to start "page"
+* on start "page" -> go -> question page
+* on question page -> options(4) -> next question -until out of questions/time-> 
+* 
+* options are in ordered list and styled
+* 
+* Other considerations: media screeen
+* Extra features (if code done by weekend)
+*/
+    
+/*      GENERAL FLOW OF SITE
+* _______________________________________________________________________________
+* on url load - starting page
+*   show prompt centered
+*   show view high score button
+*   show timer
+*   has start button (mid center) ->
+*   has view highscore (top left) ->
+*   has timer(top right; @ 000)
+*
+* on button start:
+*    get question, ?randomly?
+*    show list choices with button ->
+*    start timer counting down every second (starting from ?75) ->
+*
+* on button choice:
+*   reveal a text below, timed for 2?secs    
+*   update score;save it
+*
+*   (if more questions and time)
+*       get next question from list
+*       load its choices; ...wait for button choice
+*   (else):
+*   go to game end
+*
+* on game end
+*   get score
+*   show form intials and submit
+*
+* on submit
+*   get leaderboard
+*   place score in highest to lowest
+*
+* on view highscore
+*   load leaderboard array of initials and score
+*   on button; reset
+*   go back to home ->
+*
+* on reset
+*   delete value from local storage
+*   reload display
+*
+*/
+
+/*      REFERENCES
  * Regions <https://marketplace.visualstudio.com/items?itemName=MadsKristensen.JavaScriptRegions>
  * innerHTML vs textContent <https://www.youtube.com/watch?v=1UsllDMhvN4>
  * splice <https://www.freecodecamp.org/news/javascript-splice-how-to-use-the-splice-js-array-method/>
  * attribute - required for input tag <https://bobbyhadz.com/blog/javascript-set-attribute-required>
 */ 
 
-// TODO: Ideas for improvement/Lessons Learned: 
-/* in creation of tag or adding id to tag: add id to array to track and make removing easier;
- * a lot of extra code stem from me trying to make it more readable to me, could be less wordy, could be structured better/with less lines
- */
+/* //   TODO: Ideas for improvement/Lessons Learned:
+* in creation of tag or adding id to tag: add id to array to track and make removing easier;
+* a lot of extra code stem from me trying to make it more readable to me, could be less wordy, could be structured better/with less lines
+*/
 
 
 //Debug printer
@@ -173,10 +167,9 @@ const startString="Start Quiz";
 let questionPointer = 0; 
 
 //Submission page
+const topScoresNum = 5;//num scores shown
 const submitTitle="All done";
 const finalScore = "Your final score is "
-let leaderboard=[];
-const leaderboardSize =5;
 let player={
     'initials': "",
     'score': 0
@@ -209,7 +202,7 @@ function removeID(idString){
 }
 // #endregion
 
-// #region HTML manipulations, Specific
+// #region Other
 function createList(parentTag, itemArray){
     let newParentList = createChildTag(parentTag,"ol");
     setID(newParentList, "answers");
@@ -286,7 +279,7 @@ function stopTimer(){
 }
 // #endregion
 
-// #region Question Functions
+// #region Question Functions //TODO:(2)
 function setQuestionLayout(){
     setContent(titleID, deck[questionPointer].question);
     removeID("prompt"); removeID("start");
@@ -317,7 +310,7 @@ function getNextQuestion(){
     if(++questionPointer >= deck.length){ p('out of questions');
         stopTimer();
         setSubmissionLayout();
-    } // Case - 0 questions left
+    } // Case: 0 questions left
     else{
         setContent(titleID, deck[questionPointer].question);
         removeID("answers");
@@ -368,9 +361,9 @@ function onSubmit(event){
 //#endregion
 
 
-// #region Scoring/Leaderboard Functions
+// #region Scoring/Leaderboard Functions //TODO:(1) 
 function loadScore(){ 
-    return JSON.parse(localStorage.getItem("hiScore"));
+    return JSON.parse(localStorage.getItem("hi-score"));
 }
 function saveScore(){
     player.initials= document.getElementById("initials").value;
@@ -382,8 +375,8 @@ function saveScore(){
 
     if( previousSave == null){
         let newLeader = [player];
-        localStorage.setItem("hiScore",JSON.stringify(newLeader));
-        p("!! NEW HIGH SCORE !!");
+        localStorage.setItem("hi-score",JSON.stringify(newLeader));
+        //p("!! NEW HIGH SCORE !!");
     }
     else{
         let placement = 0;
@@ -399,30 +392,42 @@ function saveScore(){
         
         previousSave.splice(placement,0,player);
         
-        while(previousSave.length>5){
+        while(previousSave.length> topScoresNum){
              p("popping off "+ previousSave.pop());
         }
-        localStorage.setItem("hiScore",JSON.stringify(previousSave));
+        localStorage.setItem("hi-score",JSON.stringify(previousSave));
     }
     setHiScoreLayout();
-} // ! ERROR: Clicking answers to fast causes a reference error; refresh and slow down
+} // takes current score and sorts it against old scores and then saves it to local ONLY if in top range
+// ! ERROR: Clicking answers to fast causes a reference error; refresh and slow down
 
 function clearScores(){
     localStorage.clear(); //p("cleared!")
+    removeID("leaderboard");
 }
 function setHiScoreLayout(){
     hideHeader();
     removeID("submit-form");removeID("display-score");
-    let storedScores = loadScore();
+
     setContent(titleID,"High Scores");
-    //p('form removed, score loaded');
-    p(storedScores);
     createLeaderboard();
     createLeaderboardMenu();
 }
 function createLeaderboard(){
     let leaderboardArray = loadScore();
+    p(leaderboardArray);
+    
+    let leaderboard = createChildTag(mainID,"ol");
+    setID(leaderboard,"leaderboard");
+    
+    for (let i=0; i< leaderboardArray.length;i++){  //length may range 1~5
+        let initials = leaderboardArray[i].initials;
+        let score = leaderboardArray[i].score;
+        let listItem = createChildTag(leaderboard,"li");
+        setID(listItem, i); setContent(listItem,initials+ ": "+ score);
+    }   
 }
+
 function createLeaderboardMenu(){
     let menuID = createChildTag(mainID,"menu");
     setID(menuID,"menu");
